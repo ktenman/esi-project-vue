7b15f5218bd2c34f8f6fccf58c5e6fe5d1ae6365
ZIya Mammadov <55743413+ziyamammadov@users.noreply.github.com>
2022-05-14 22:35:21 +0300

    fixed the bug

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://esitartulibrary.herokuapp.com/users';
+const API_URL = 'https://esitartulibrary.herokuapp.com/users/';
 // const API_URL = 'http://localhost:8081/users/';
 
 
