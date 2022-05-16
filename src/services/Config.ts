dbae79eba74a7046df8437f5b2ef4e346e6222f1
ZIya Mammadov <55743413+ziyamammadov@users.noreply.github.com>
2022-05-16 09:31:03 +0300

    changed the url

--- src/services/Config.ts
+++ src/services/Config.ts
@@ -1,2 +1,2 @@
-// export const API_URL = 'https://esitartulibrary.herokuapp.com';
-export const API_URL = 'http://localhost:8081';
+export const API_URL = 'https://esitartulibrary.herokuapp.com';
+// export const API_URL = 'http://localhost:8081';
